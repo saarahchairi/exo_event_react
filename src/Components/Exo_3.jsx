dd7bad8213c16404d2e93744fd6b44c50e9fc04a
saarahchairi <sarni555@hotmail.com>
2022-07-07 10:10:09 +0200

    exo 4 done

--- src/Components/Exo_3.jsx
+++ src/Components/Exo_3.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Exo3() {
     return (
-        <div>Exo3</div>
+        <div>
+            <h2>Exercice 3</h2>
+            <input type="text" />
+        </div>
     )
 }
--- src/Components/Exo_4.jsx
+++ src/Components/Exo_4.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Exo4() {
     return (
-        <div>Exo4</div>
+        <div>
+            <h2>Exercice 4</h2>
+            <input type="text" onCopy={()=>alert("Texte copié!")} />
+        </div>
     )
 }
