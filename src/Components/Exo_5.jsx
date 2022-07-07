82f919104d881f9abca3d5e0ef6bc5309fc1f4d6
saarahchairi <sarni555@hotmail.com>
2022-07-07 10:04:22 +0200

    exo 5 done

--- src/Components/Exo_5.jsx
+++ src/Components/Exo_5.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Exo5() {
     return (
-        <div>Exo5</div>
+        <div>
+            <h2>Exercice 5</h2>
+            <button onClick={() => alert("C'était pour voir si tu suivais bien ;)")}>Clique</button>
+        </div>
     )
 }
