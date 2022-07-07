70cd26b99577280a8825a8239ee0e86cf75cd692
saarahchairi <sarni555@hotmail.com>
2022-07-07 10:13:50 +0200

    exo 3 done

--- src/Components/Exo_3.jsx
+++ src/Components/Exo_3.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default function Exo3() {
+    let changeColorFocus = (e) => {
+        e.target.style.backgroundColor = "green"
+    }
+    let retour = (e) => {
+        e.target.style.backgroundColor = ""
+    }
     return (
         <div>
             <h2>Exercice 3</h2>
-            <input type="text" />
+            <input type="text" onFocus={changeColorFocus} onBlur={retour} />
         </div>
     )
 }
